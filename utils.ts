ec99b3f4368f6754f1fc4f9c754ef5b8b412bf28
Mykola Skrypets <skrypets@gmail.com>
2024-10-23 00:41:39 +0200

    fix: Stringify commands in DiscordRequest body

--- utils.ts
+++ utils.ts
@@ -43,9 +43,9 @@ export async function InstallGlobalCommands(
   const endpoint = `applications/${appId}/commands`;
 
   try {
-    await DiscordRequest(endpoint, { 
-      method: "PUT", 
-      body: commands 
+    await DiscordRequest(endpoint, {
+      method: "PUT",
+      body: JSON.stringify(commands),
     });
     console.log("Successfully installed global commands");
   } catch (err) {
