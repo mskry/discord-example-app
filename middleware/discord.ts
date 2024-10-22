1a9426299cdbf737870756191f75bcf7c89b3675
Mykola Skrypets <skrypets@gmail.com>
2024-10-22 21:48:44 +0200

    feat: Add logging for Discord request validation

--- middleware/discord.ts
+++ middleware/discord.ts
@@ -7,8 +7,14 @@ export const verifyDiscordRequest = (clientPublicKey: string) =>
     const signature = c.req.header("X-Signature-Ed25519");
     const timestamp = c.req.header("X-Signature-Timestamp");
     const rawBody = await c.req.raw.text();
+    const isValidRequest = await verifyKey(
+      rawBody,
+      signature,
+      timestamp,
+      clientPublicKey,
+    );
 
-    const isValidRequest = await verifyKey(rawBody, signature, timestamp, clientPublicKey);
+    console.log(isValidRequest);
     if (!isValidRequest) {
       return c.text("Bad request signature", 401);
     }
