ed6be4a92d1246333bccb8ee3e38ad150425c680
Mykola Skrypets <skrypets@gmail.com>
2024-10-22 21:23:02 +0200

    feat: Add request validity logging in Discord middleware

--- middleware/discord.ts
+++ middleware/discord.ts
@@ -8,37 +8,39 @@ import type { Context, Next } from "hono";
  * @param clientPublicKey - The public key from the Discord developer dashboard
  * @returns The middleware function
  */
-export const verifyDiscordRequest = (clientPublicKey: string) => createMiddleware(
-  async (c: Context, next: Next) => {
-    const signature = c.req.raw.headers.get("X-Signature-Ed25519");
-    const timestamp = c.req.raw.headers.get("X-Signature-Timestamp");
+export const verifyDiscordRequest = (clientPublicKey: string) =>
+  createMiddleware(
+    async (c: Context, next: Next) => {
+      const signature = c.req.raw.headers.get("X-Signature-Ed25519");
+      const timestamp = c.req.raw.headers.get("X-Signature-Timestamp");
 
-    if (!signature || !timestamp) {
-      return c.text("Missing request signature", 401);
-    }
-
-    const rawBody = await c.req.raw.text();
-
-    try {
-      const isValidRequest = verifyKey(
-        rawBody,
-        signature,
-        timestamp,
-        clientPublicKey,
-      );
-      if (!isValidRequest) {
-        return c.text("Invalid request signature", 401);
+      if (!signature || !timestamp) {
+        return c.text("Missing request signature", 401);
       }
-    } catch (error) {
-      console.error("Error verifying request:", error);
-      return c.text("Error verifying request", 401);
-    }
 
-    // Parse the body and attach it to the context
-    c.set("parsedBody", JSON.parse(rawBody));
-    console.log(JSON.parse(rawBody));
+      const rawBody = await c.req.raw.text();
 
-    // Ensure we call next() and return its result
-    return next();
-  },
-);
+      try {
+        const isValidRequest = verifyKey(
+          rawBody,
+          signature,
+          timestamp,
+          clientPublicKey,
+        );
+        console.log("req validity :", isValidRequest);
+        if (!isValidRequest) {
+          return c.text("Invalid request signature", 401);
+        }
+      } catch (error) {
+        console.error("Error verifying request:", error);
+        return c.text("Error verifying request", 401);
+      }
+
+      // Parse the body and attach it to the context
+      c.set("parsedBody", JSON.parse(rawBody));
+      console.log(JSON.parse(rawBody));
+
+      // Ensure we call next() and return its result
+      return next();
+    },
+  );
